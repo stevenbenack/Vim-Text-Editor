97f95960ee55680c9bb5518f68aa44ef709a6a38
Steven Benack <stevenbenack@users.noreply.github.com>
2017-02-19 00:40:32 -0600

    Wrote an intializer for the buffer

--- Text-Editor/buffer.c
+++ Text-Editor/buffer.c
@@ -1,5 +1,15 @@
 #include "buffer.h"
 
+/**
+* Initalizes a buffer
+* @param  name file name to initalize the buffer with (can be null)
+* @return      pointer to the initalized buffer
+*/
 buffer_t* buffer_create(const char * name) {
-
+	buffer_t* buf = calloc(1, sizeof(buffer_t)); 
+	if (name) {
+		buf->name = strdup(name); //duplicate string
+	}
+	buffer_read(buf, name);
+	return buf;
 }
